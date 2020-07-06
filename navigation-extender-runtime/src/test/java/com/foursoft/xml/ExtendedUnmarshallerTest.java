e89fed0c164d623c9961d2a54d03ed11c54d61ef
Roland Spatzenegger <spatzenegger@4soft.de>
2020-07-06 18:11:19 +0200

    remove testng, streamline poms

--- navigation-extender-runtime/src/test/java/com/foursoft/xml/ExtendedUnmarshallerTest.java
+++ navigation-extender-runtime/src/test/java/com/foursoft/xml/ExtendedUnmarshallerTest.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,14 +27,13 @@ package com.foursoft.xml;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import javax.xml.bind.JAXBException;
-
-import org.testng.annotations.Test;
-
 import com.foursoft.test.model.AbstractBase;
 import com.foursoft.test.model.ChildA;
 import com.foursoft.test.model.ChildB;
 import com.foursoft.test.model.Root;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.bind.JAXBException;
 
 public class ExtendedUnmarshallerTest {
 
