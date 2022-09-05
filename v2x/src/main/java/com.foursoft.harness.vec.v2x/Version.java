fc89996133bb6ad0992fa5ecb71e536ffb44c7ac
Johannes Becker <johannes.becker@4soft.de>
2022-09-05 08:46:25 +0200

    Fixed error in Version class.

--- v2x/src/main/java/com.foursoft.harness.vec.v2x/Version.java
+++ v2x/src/main/java/com.foursoft.harness.vec.v2x/Version.java
@@ -26,7 +26,7 @@
 package com.foursoft.harness.vec.v2x;
 
 public final class Version {
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "2.0.0";
 
     private Version() {
     }
