d4a13465ecdddb6e675c64a9035d3343a63a315d
DManstrator <DManstrator@gmail.com>
2024-11-26 14:25:35 +0100

    Removed unneeded test methods

--- compatibility/compatibility-vec11to12/src/test/java/com/foursoft/harness/compatibility/vec11to12/wrapper/vec11to12/specification/Vec11To12StripeSpecificationWrapperTest.java
+++ compatibility/compatibility-vec11to12/src/test/java/com/foursoft/harness/compatibility/vec11to12/wrapper/vec11to12/specification/Vec11To12StripeSpecificationWrapperTest.java
@@ -82,39 +82,4 @@ class Vec11To12StripeSpecificationWrapperTest extends AbstractBaseWrapperTest {
         }
     }
 
-    private com.foursoft.harness.vec.v113.VecContent prepareVec113() {
-        final com.foursoft.harness.vec.v113.VecContent vecContent = new com.foursoft.harness.vec.v113.VecContent();
-        vecContent.setXmlId("vec_content_1");
-        vecContent.setVecVersion("1.1.3");
-
-        final com.foursoft.harness.vec.v113.VecDocumentVersion vecDocumentVersion =
-                new com.foursoft.harness.vec.v113.VecDocumentVersion();
-        vecDocumentVersion.setXmlId("vec_document_version_1");
-
-        final com.foursoft.harness.vec.v113.VecStripeSpecification vecStripeSpecification =
-                new com.foursoft.harness.vec.v113.VecStripeSpecification();
-        vecStripeSpecification.setXmlId("vec_stripe_specification_1");
-        vecStripeSpecification.setIdentification("Stripe");
-        vecStripeSpecification.getCustomProperties().add(createCustomProperty());
-
-        vecDocumentVersion.getSpecifications().add(vecStripeSpecification);
-        vecContent.getDocumentVersions().add(vecDocumentVersion);
-        return vecContent;
-    }
-
-    private com.foursoft.harness.vec.v113.VecCustomProperty createCustomProperty() {
-        final com.foursoft.harness.vec.v113.VecNumericalValueProperty vecNumericalValueProperty =
-                new com.foursoft.harness.vec.v113.VecNumericalValueProperty();
-        vecNumericalValueProperty.setXmlId("vec_numerical_value_property_1");
-        vecNumericalValueProperty.setPropertyType("Thickness");
-
-        final com.foursoft.harness.vec.v113.VecNumericalValue vecNumericalValue =
-                new com.foursoft.harness.vec.v113.VecNumericalValue();
-        vecNumericalValue.setXmlId("vec_numerical_value_property_2");
-        vecNumericalValue.setValueComponent(1.0);
-
-        vecNumericalValueProperty.setValue(vecNumericalValue);
-        return vecNumericalValueProperty;
-    }
-
 }
