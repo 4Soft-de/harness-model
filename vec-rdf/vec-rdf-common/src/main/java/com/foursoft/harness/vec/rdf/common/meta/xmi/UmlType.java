d7801e687202b06388feceb1ec62d3a49ea588f6
Johannes Becker <johannes.becker@4soft.de>
2024-12-09 20:44:31 +0100

    Fixed sonar warnings.

--- vec-rdf/vec-rdf-common/src/main/java/com/foursoft/harness/vec/rdf/common/meta/xmi/UmlType.java
+++ vec-rdf/vec-rdf-common/src/main/java/com/foursoft/harness/vec/rdf/common/meta/xmi/UmlType.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,6 +24,9 @@
  * =========================LICENSE_END==================================
  */
 package com.foursoft.harness.vec.rdf.common.meta.xmi;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public record UmlType(String xmiId, String name, String xmiType, UmlLiteral[] literals) {
 
@@ -39,4 +42,21 @@ public record UmlType(String xmiId, String name, String xmiType, UmlLiteral[] li
         return VecModelProviderBuilder.UML_PRIMITIVE.equals(xmiType);
     }
 
+    @Override public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final UmlType umlType = (UmlType) o;
+        return Objects.equals(name, umlType.name) && Objects.equals(xmiId, umlType.xmiId) &&
+                Objects.equals(xmiType, umlType.xmiType) && Arrays.equals(literals, umlType.literals);
+    }
+
+    @Override public int hashCode() {
+        int result = Objects.hashCode(xmiId);
+        result = 31 * result + Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(xmiType);
+        result = 31 * result + Arrays.hashCode(literals);
+        return result;
+    }
 }
