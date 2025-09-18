f57b1e5aab9a9e9be350ea43ad45ddea5582956a
Yousif Yacoub <yacoub@4soft.de>
2025-09-18 09:29:47 +0200

    Add test for CavityTransformer

---
+++ kbl2vec/src/test/java/com/foursoft/harness/kbl2vec/transform/components/connector/CavityTransformerTest.java
@@ -0,0 +1,27 @@
+package com.foursoft.harness.kbl2vec.transform.components.connector;
+
+import com.foursoft.harness.kbl.v25.KblCavity;
+import com.foursoft.harness.kbl2vec.core.TestConversionOrchestrator;
+import com.foursoft.harness.vec.v2x.VecCavity;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CavityTransformerTest {
+
+    @Test
+    public void should_transformCavity() {
+        // Given
+        final CavityTransformer transformer = new CavityTransformer();
+        final TestConversionOrchestrator orchestrator = new TestConversionOrchestrator();
+
+        final KblCavity source = new KblCavity();
+        source.setCavityNumber("TestCavityNumber");
+
+        // When
+        final VecCavity result = orchestrator.transform(transformer, source);
+
+        // Then
+        assertThat(result).isNotNull().returns("TestCavityNumber", VecCavity::getCavityNumber);
+    }
+}
