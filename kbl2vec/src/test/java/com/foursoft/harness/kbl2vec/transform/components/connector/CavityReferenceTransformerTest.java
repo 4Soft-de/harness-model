d766f6a05bdcdd8091850aa1901472919d49a771
Yousif Yacoub <yacoub@4soft.de>
2025-09-18 09:18:46 +0200

    Add test for CavityReferenceTransformer

---
+++ kbl2vec/src/test/java/com/foursoft/harness/kbl2vec/transform/components/connector/CavityReferenceTransformerTest.java
@@ -0,0 +1,37 @@
+package com.foursoft.harness.kbl2vec.transform.components.connector;
+
+import com.foursoft.harness.kbl.v25.KblCavity;
+import com.foursoft.harness.kbl.v25.KblCavityOccurrence;
+import com.foursoft.harness.kbl2vec.core.TestConversionOrchestrator;
+import com.foursoft.harness.vec.v2x.VecCavity;
+import com.foursoft.harness.vec.v2x.VecCavityReference;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CavityReferenceTransformerTest {
+
+    @Test
+    public void should_transformCavityReference() {
+        // Given
+        final CavityReferenceTransformer transformer = new CavityReferenceTransformer();
+        final TestConversionOrchestrator orchestrator = new TestConversionOrchestrator();
+
+        final KblCavity part = new KblCavity();
+        part.setCavityNumber("TestCavityNumber");
+
+        final KblCavityOccurrence source = new KblCavityOccurrence();
+        source.setPart(part);
+
+        final VecCavity vecCavity = new VecCavity();
+        orchestrator.addMockMapping(part, vecCavity);
+
+        // When
+        final VecCavityReference result = orchestrator.transform(transformer, source);
+
+        // Then
+        assertThat(result).isNotNull()
+                .returns("TestCavityNumber", VecCavityReference::getIdentification)
+                .returns(vecCavity, VecCavityReference::getReferencedCavity);
+    }
+}
