a971035a2813b6ee0593c3832376d4c63198db25
Yousif Yacoub <yacoub@4soft.de>
2025-09-22 16:52:40 +0200

    Implement and test WireProtectionDocumentVersionTransformer

---
+++ kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/components/wires/WireProtectionDocumentVersionTransformer.java
@@ -0,0 +1,31 @@
+package com.foursoft.harness.kbl2vec.transform.components.wires;
+
+import com.foursoft.harness.kbl.v25.KblPart;
+import com.foursoft.harness.kbl.v25.KblWireProtection;
+import com.foursoft.harness.kbl2vec.core.Query;
+import com.foursoft.harness.kbl2vec.core.TransformationContext;
+import com.foursoft.harness.kbl2vec.core.TransformationResult;
+import com.foursoft.harness.kbl2vec.core.Transformer;
+import com.foursoft.harness.vec.v2x.VecDocumentVersion;
+import com.foursoft.harness.vec.v2x.VecWireProtectionSpecification;
+
+import static com.foursoft.harness.kbl2vec.transform.components.common.Fragments.commonComponentInformation;
+
+public class WireProtectionDocumentVersionTransformer implements Transformer<KblPart, VecDocumentVersion> {
+
+    @Override
+    public TransformationResult<VecDocumentVersion> transform(final TransformationContext context,
+                                                              final KblPart kblPart) {
+        if (kblPart instanceof final KblWireProtection source) {
+            final VecDocumentVersion destination = new VecDocumentVersion();
+
+            return TransformationResult
+                    .from(destination)
+                    .withFragment(commonComponentInformation(source, context))
+                    .withDownstream(KblWireProtection.class, VecWireProtectionSpecification.class, Query.of(source),
+                                    VecDocumentVersion::getSpecifications)
+                    .build();
+        }
+        return TransformationResult.noResult();
+    }
+}
---
+++ kbl2vec/src/test/java/com/foursoft/harness/kbl2vec/transform/components/wires/WireProtectionDocumentVersionTransformerTest.java
@@ -0,0 +1,47 @@
+package com.foursoft.harness.kbl2vec.transform.components.wires;
+
+import com.foursoft.harness.kbl.v25.KblWireProtection;
+import com.foursoft.harness.kbl2vec.core.TestConversionOrchestrator;
+import com.foursoft.harness.vec.v2x.VecDocumentVersion;
+import com.foursoft.harness.vec.v2x.VecGeneralTechnicalPartSpecification;
+import com.foursoft.harness.vec.v2x.VecPartVersion;
+import com.foursoft.harness.vec.v2x.VecWireProtectionSpecification;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WireProtectionDocumentVersionTransformerTest {
+
+    @Test
+    void should_transformWireProtectionDocumentVersion() {
+        // Given
+        final WireProtectionDocumentVersionTransformer transformer = new WireProtectionDocumentVersionTransformer();
+        final TestConversionOrchestrator orchestrator = new TestConversionOrchestrator();
+
+        final KblWireProtection source = new KblWireProtection();
+        source.setCompanyName("TestCompany");
+        source.setVersion("TestVersion");
+
+        final VecGeneralTechnicalPartSpecification vecGeneralSpecification = new VecGeneralTechnicalPartSpecification();
+        final VecWireProtectionSpecification vecWireProtectionSpecification = new VecWireProtectionSpecification();
+
+        orchestrator.addMockMapping(source, vecGeneralSpecification);
+        orchestrator.addMockMapping(source, vecWireProtectionSpecification);
+
+        final VecPartVersion vecPartVersion = new VecPartVersion();
+
+        orchestrator.addMockMapping(source, vecPartVersion);
+
+        // When
+        final VecDocumentVersion result = orchestrator.transform(transformer, source);
+
+        // Then
+        assertThat(result)
+                .isNotNull()
+                .returns("TestCompany", VecDocumentVersion::getCompanyName)
+                .returns("TestVersion", VecDocumentVersion::getDocumentVersion)
+                .satisfies(v -> assertThat(v.getSpecifications()).containsExactlyInAnyOrder(vecGeneralSpecification,
+                                                                                            vecWireProtectionSpecification))
+                .satisfies(v -> assertThat(v.getReferencedPart()).containsExactly(vecPartVersion));
+    }
+}
