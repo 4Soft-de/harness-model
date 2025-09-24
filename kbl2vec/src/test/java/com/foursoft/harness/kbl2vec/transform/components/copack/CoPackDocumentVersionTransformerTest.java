d9a921bf44204ba487587e5752002a2e10e036a8
Yousif Yacoub <yacoub@4soft.de>
2025-09-24 09:29:43 +0200

    Fix test

--- kbl2vec/src/test/java/com/foursoft/harness/kbl2vec/transform/components/copack/CoPackDocumentVersionTransformerTest.java
+++ kbl2vec/src/test/java/com/foursoft/harness/kbl2vec/transform/components/copack/CoPackDocumentVersionTransformerTest.java
@@ -4,6 +4,7 @@ import com.foursoft.harness.kbl.v25.KblCoPackPart;
 import com.foursoft.harness.kbl2vec.core.TestConversionOrchestrator;
 import com.foursoft.harness.vec.v2x.VecDocumentVersion;
 import com.foursoft.harness.vec.v2x.VecGeneralTechnicalPartSpecification;
+import com.foursoft.harness.vec.v2x.VecPartOrUsageRelatedSpecification;
 import com.foursoft.harness.vec.v2x.VecPartVersion;
 import org.junit.jupiter.api.Test;
 
@@ -13,24 +14,31 @@ class CoPackDocumentVersionTransformerTest {
 
     @Test
     void should_transformCoPackDocumentVersion() {
+        // Given
         final CoPackDocumentVersionTransformer transformer = new CoPackDocumentVersionTransformer();
         final TestConversionOrchestrator orchestrator = new TestConversionOrchestrator();
 
         final KblCoPackPart source = new KblCoPackPart();
         source.setVersion("TestVersion");
 
-        final VecGeneralTechnicalPartSpecification vecSpecification = new VecGeneralTechnicalPartSpecification();
+        final VecPartOrUsageRelatedSpecification vecSpecification = new VecPartOrUsageRelatedSpecification();
+        final VecGeneralTechnicalPartSpecification vecGeneralSpecification = new VecGeneralTechnicalPartSpecification();
+
+        orchestrator.addMockMapping(source, vecGeneralSpecification);
         orchestrator.addMockMapping(source, vecSpecification);
 
         final VecPartVersion vecPartVersion = new VecPartVersion();
         orchestrator.addMockMapping(source, vecPartVersion);
 
+        // When
         final VecDocumentVersion result = orchestrator.transform(transformer, source);
 
+        // Then
         assertThat(result).isNotNull()
                 .returns("PartMaster", VecDocumentVersion::getDocumentType)
                 .returns("TestVersion", VecDocumentVersion::getDocumentVersion)
-                .satisfies(v -> assertThat(v.getSpecifications()).containsExactly(vecSpecification))
-                .satisfies(v -> assertThat(v.getReferencedPart()).containsExactly(vecPartVersion));
+                .satisfies(v -> assertThat(v.getReferencedPart()).containsExactly(vecPartVersion))
+                .satisfies(v -> assertThat(v.getSpecifications()).containsExactlyInAnyOrder(vecSpecification,
+                                                                                            vecGeneralSpecification));
     }
 }
