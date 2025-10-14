e1bf91f98ee2a7fd39c9998c86d6ccea6ad49c4e
Yousif Yacoub <yacoub@4soft.de>
2025-10-14 17:13:37 +0200

    Improve transformer class for better readability

--- kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/harness/ContactingSpecificationTransformer.java
+++ kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/harness/ContactingSpecificationTransformer.java
@@ -27,7 +27,6 @@ package com.foursoft.harness.kbl2vec.transform.harness;
 
 import com.foursoft.harness.kbl.v25.KblContactPoint;
 import com.foursoft.harness.kbl.v25.KblHarness;
-import com.foursoft.harness.kbl2vec.core.Query;
 import com.foursoft.harness.kbl2vec.core.TransformationContext;
 import com.foursoft.harness.kbl2vec.core.TransformationResult;
 import com.foursoft.harness.kbl2vec.core.Transformer;
@@ -47,11 +46,15 @@ public class ContactingSpecificationTransformer implements Transformer<KblHarnes
         destination.setIdentification(
                 abbreviatedClassName(destination.getClass()) + "-" + source.getPartNumber());
 
-        final List<KblContactPoint> contactPoints = source.getConnectorOccurrences().stream().flatMap(
-                c -> c.getContactPoints().stream()).toList();
         return TransformationResult.from(destination)
-                .withDownstream(KblContactPoint.class, VecContactPoint.class, Query.fromLists(contactPoints),
+                .withDownstream(KblContactPoint.class, VecContactPoint.class, () -> getContactPoints(source),
                                 VecContactingSpecification::getContactPoints)
                 .build();
+    }
+
+    private List<KblContactPoint> getContactPoints(final KblHarness harness) {
+        return harness.getConnectorOccurrences().stream()
+                .flatMap(c -> c.getContactPoints().stream())
+                .toList();
     }
 }
