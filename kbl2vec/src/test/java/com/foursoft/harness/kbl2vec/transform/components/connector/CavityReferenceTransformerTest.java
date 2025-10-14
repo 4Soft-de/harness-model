80cfe478669c93cbbd744de7d78293d7b02b97a1
Yousif Yacoub <yacoub@4soft.de>
2025-10-14 10:02:09 +0200

    Adapt test to check for custom properties

--- kbl2vec/src/test/java/com/foursoft/harness/kbl2vec/transform/components/connector/CavityReferenceTransformerTest.java
+++ kbl2vec/src/test/java/com/foursoft/harness/kbl2vec/transform/components/connector/CavityReferenceTransformerTest.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,9 +27,12 @@ package com.foursoft.harness.kbl2vec.transform.components.connector;
 
 import com.foursoft.harness.kbl.v25.KblCavity;
 import com.foursoft.harness.kbl.v25.KblCavityOccurrence;
+import com.foursoft.harness.kbl.v25.KblProcessingInstruction;
 import com.foursoft.harness.kbl2vec.core.TestConversionOrchestrator;
 import com.foursoft.harness.vec.v2x.VecCavity;
 import com.foursoft.harness.vec.v2x.VecCavityReference;
+import com.foursoft.harness.vec.v2x.VecCustomProperty;
+import com.foursoft.harness.vec.v2x.VecSimpleValueProperty;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -51,12 +54,19 @@ class CavityReferenceTransformerTest {
         final VecCavity vecCavity = new VecCavity();
         orchestrator.addMockMapping(part, vecCavity);
 
+        final KblProcessingInstruction processingInstruction = new KblProcessingInstruction();
+        source.getProcessingInformations().add(processingInstruction);
+
+        final VecCustomProperty customProperty = new VecSimpleValueProperty();
+        orchestrator.addMockMapping(processingInstruction, customProperty);
+
         // When
         final VecCavityReference result = orchestrator.transform(transformer, source);
 
         // Then
         assertThat(result).isNotNull()
                 .returns("TestCavityNumber", VecCavityReference::getIdentification)
-                .returns(vecCavity, VecCavityReference::getReferencedCavity);
+                .returns(vecCavity, VecCavityReference::getReferencedCavity)
+                .satisfies(v -> assertThat(v.getCustomProperties()).contains(customProperty));
     }
 }
