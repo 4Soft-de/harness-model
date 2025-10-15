937d833da60830b022e3ed5750a6d8184b0b8bd5
Yousif Yacoub <yacoub@4soft.de>
2025-10-15 17:06:23 +0200

    Sort extremities for snapshot test determinability

--- kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/connectivity/WireMountingTransformer.java
+++ kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/connectivity/WireMountingTransformer.java
@@ -2,6 +2,7 @@ package com.foursoft.harness.kbl2vec.transform.connectivity;
 
 import com.foursoft.harness.kbl.v25.KblCavitySealOccurrence;
 import com.foursoft.harness.kbl.v25.KblContactPoint;
+import com.foursoft.harness.kbl.v25.KblExtremity;
 import com.foursoft.harness.kbl2vec.core.Query;
 import com.foursoft.harness.kbl2vec.core.TransformationContext;
 import com.foursoft.harness.kbl2vec.core.TransformationResult;
@@ -11,6 +12,7 @@ import com.foursoft.harness.vec.v2x.VecCavitySealRole;
 import com.foursoft.harness.vec.v2x.VecWireEnd;
 import com.foursoft.harness.vec.v2x.VecWireMounting;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class WireMountingTransformer implements Transformer<KblContactPoint, VecWireMounting> {
@@ -23,9 +25,15 @@ public class WireMountingTransformer implements Transformer<KblContactPoint, Vec
         return TransformationResult.from(destination)
                 .withLinker(Query.fromLists(getCavitySealOccurrences(source)), VecCavitySealRole.class,
                             VecWireMounting::setMountedCavitySeal)
-                .withLinker(Query.fromLists(source.getRefExtremity().stream().toList()), VecWireEnd.class,
+                .withLinker(Query.fromLists(getSortedExtremities(source)), VecWireEnd.class,
                             VecWireMounting::getReferencedWireEnd)
                 .build();
+    }
+
+    private List<KblExtremity> getSortedExtremities(final KblContactPoint contactPoint) {
+        return contactPoint.getRefExtremity().stream()
+                .sorted(Comparator.comparing(KblExtremity::getXmlId))
+                .toList();
     }
 
     private List<KblCavitySealOccurrence> getCavitySealOccurrences(final KblContactPoint contactPoint) {
