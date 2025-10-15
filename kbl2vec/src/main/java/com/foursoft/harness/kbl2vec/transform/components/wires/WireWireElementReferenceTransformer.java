f4c9b89535b80145cf7179f85f003e144a5e381e
Yousif Yacoub <56478803+yoshi-ya@users.noreply.github.com>
2025-10-15 12:03:40 +0200

    [kbl2vec] Transform wire ends (#371)
    Co-authored-by: Johannes Becker <johannes.becker@4soft.de>

--- kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/components/wires/WireWireElementReferenceTransformer.java
+++ kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/components/wires/WireWireElementReferenceTransformer.java
@@ -25,17 +25,18 @@
  */
 package com.foursoft.harness.kbl2vec.transform.components.wires;
 
-import com.foursoft.harness.kbl.v25.KblGeneralWireOccurrence;
-import com.foursoft.harness.kbl.v25.KblSpecialWireOccurrence;
-import com.foursoft.harness.kbl.v25.KblWireLength;
-import com.foursoft.harness.kbl.v25.KblWireOccurrence;
+import com.foursoft.harness.kbl.v25.*;
 import com.foursoft.harness.kbl2vec.core.Query;
 import com.foursoft.harness.kbl2vec.core.TransformationContext;
 import com.foursoft.harness.kbl2vec.core.TransformationResult;
 import com.foursoft.harness.kbl2vec.core.Transformer;
 import com.foursoft.harness.vec.v2x.VecWireElement;
 import com.foursoft.harness.vec.v2x.VecWireElementReference;
+import com.foursoft.harness.vec.v2x.VecWireEnd;
 import com.foursoft.harness.vec.v2x.VecWireLength;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WireWireElementReferenceTransformer
         implements Transformer<KblGeneralWireOccurrence, VecWireElementReference> {
@@ -44,6 +45,7 @@ public class WireWireElementReferenceTransformer
     public TransformationResult<VecWireElementReference> transform(final TransformationContext context,
                                                                    final KblGeneralWireOccurrence source) {
         final VecWireElementReference dest = new VecWireElementReference();
+
         if (source instanceof final KblWireOccurrence wireOccurrence) {
             dest.setIdentification(wireOccurrence.getWireNumber());
         } else if (source instanceof final KblSpecialWireOccurrence specialWireOccurrence) {
@@ -51,11 +53,23 @@ public class WireWireElementReferenceTransformer
         } else {
             context.getLogger().warn("'{}' has a unsupported wire class type", source);
         }
+
         return TransformationResult.from(dest)
                 .withDownstream(KblWireLength.class, VecWireLength.class, source::getLengthInformations,
                                 VecWireElementReference::getWireLengths)
+                .withDownstream(KblExtremity.class, VecWireEnd.class, () -> getExtremities(source),
+                                VecWireElementReference::getWireEnds)
                 .withLinker(Query.of(source::getPart), VecWireElement.class,
                             VecWireElementReference::setReferencedWireElement)
                 .build();
+    }
+
+    private List<KblExtremity> getExtremities(final KblGeneralWireOccurrence source) {
+        if (source instanceof final KblWireOccurrence wireOccurrence) {
+            return wireOccurrence.getRefConnection().stream()
+                    .flatMap(v -> v.getExtremities().stream())
+                    .toList();
+        }
+        return new ArrayList<>();
     }
 }
