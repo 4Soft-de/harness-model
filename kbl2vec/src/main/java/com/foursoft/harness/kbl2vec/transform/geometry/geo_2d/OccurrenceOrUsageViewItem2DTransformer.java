be178608244adda59074f085378bb82c09efa78c
Yousif Yacoub <yacoub@4soft.de>
2025-10-24 13:24:22 +0200

    Add log for missing ids and improve structure

--- kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/geometry/geo_2d/OccurrenceOrUsageViewItem2DTransformer.java
+++ kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/geometry/geo_2d/OccurrenceOrUsageViewItem2DTransformer.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,6 +26,7 @@
 package com.foursoft.harness.kbl2vec.transform.geometry.geo_2d;
 
 import com.foursoft.harness.kbl.common.HasAliasId;
+import com.foursoft.harness.kbl.common.HasIdentification;
 import com.foursoft.harness.kbl.common.HasPlacement;
 import com.foursoft.harness.kbl.v25.ConnectionOrOccurrence;
 import com.foursoft.harness.kbl.v25.KblAliasIdentification;
@@ -39,6 +40,7 @@ import com.foursoft.harness.vec.v2x.VecAliasIdentification;
 import com.foursoft.harness.vec.v2x.VecOccurrenceOrUsageViewItem2D;
 import com.foursoft.harness.vec.v2x.VecPartOccurrence;
 import com.foursoft.harness.vec.v2x.VecTransformation2D;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -48,29 +50,36 @@ public class OccurrenceOrUsageViewItem2DTransformer
     @Override
     public TransformationResult<VecOccurrenceOrUsageViewItem2D> transform(final TransformationContext context,
                                                                           final ConnectionOrOccurrence source) {
-        final VecOccurrenceOrUsageViewItem2D destination = new VecOccurrenceOrUsageViewItem2D();
-        destination.setIdentification("ViewItem2D_" + source.getXmlId());
-
-        final TransformationResult.Builder<VecOccurrenceOrUsageViewItem2D> builder = TransformationResult.from(
-                destination);
-
         if (source instanceof final HasPlacement<?> hasPlacement) {
+            final VecOccurrenceOrUsageViewItem2D destination = new VecOccurrenceOrUsageViewItem2D();
+
+            if (source instanceof final HasIdentification hasIdentification && StringUtils.isNotBlank(
+                    hasIdentification.getId())) {
+                destination.setIdentification(hasIdentification.getId());
+            } else {
+                context.getLogger().warn("Occurrence (xml ID: {}) has no ID.", source.getXmlId());
+                destination.setIdentification("ViewItem2D_" + source.getXmlId());
+            }
+
+            final TransformationResult.Builder<VecOccurrenceOrUsageViewItem2D> builder = TransformationResult.from(
+                    destination);
+
+            if (source instanceof final HasAliasId<?> hasAliasId) {
+                final List<KblAliasIdentification> aliasIds = hasAliasId.getAliasIds().stream()
+                        .flatMap(StreamUtils.ofClass(KblAliasIdentification.class))
+                        .toList();
+                builder.withDownstream(KblAliasIdentification.class, VecAliasIdentification.class,
+                                       Query.fromLists(aliasIds), VecOccurrenceOrUsageViewItem2D::getAliasIds);
+            }
+
             final KblTransformation transformation = (KblTransformation) hasPlacement.getPlacement();
-            builder.withDownstream(KblTransformation.class, VecTransformation2D.class, Query.of(transformation),
-                                   VecOccurrenceOrUsageViewItem2D::setOrientation);
+            return builder
+                    .withDownstream(KblTransformation.class, VecTransformation2D.class, Query.of(transformation),
+                                    VecOccurrenceOrUsageViewItem2D::setOrientation)
+                    .withLinker(Query.of(source), VecPartOccurrence.class,
+                                VecOccurrenceOrUsageViewItem2D::getOccurrenceOrUsage)
+                    .build();
         }
-
-        if (source instanceof final HasAliasId<?> hasAliasId) {
-            final List<KblAliasIdentification> aliasIds = hasAliasId.getAliasIds().stream()
-                    .flatMap(StreamUtils.ofClass(KblAliasIdentification.class))
-                    .toList();
-            builder.withDownstream(KblAliasIdentification.class, VecAliasIdentification.class,
-                                   Query.fromLists(aliasIds), VecOccurrenceOrUsageViewItem2D::getAliasIds);
-        }
-
-        return builder
-                .withLinker(Query.of(source), VecPartOccurrence.class,
-                            VecOccurrenceOrUsageViewItem2D::getOccurrenceOrUsage)
-                .build();
+        return TransformationResult.noResult();
     }
 }
--- kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/geometry/geo_3d/OccurrenceOrUsageViewItem3DTransformer.java
+++ kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/geometry/geo_3d/OccurrenceOrUsageViewItem3DTransformer.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,6 +26,7 @@
 package com.foursoft.harness.kbl2vec.transform.geometry.geo_3d;
 
 import com.foursoft.harness.kbl.common.HasAliasId;
+import com.foursoft.harness.kbl.common.HasIdentification;
 import com.foursoft.harness.kbl.common.HasPlacement;
 import com.foursoft.harness.kbl.v25.ConnectionOrOccurrence;
 import com.foursoft.harness.kbl.v25.KblAliasIdentification;
@@ -39,6 +40,7 @@ import com.foursoft.harness.vec.v2x.VecAliasIdentification;
 import com.foursoft.harness.vec.v2x.VecOccurrenceOrUsageViewItem3D;
 import com.foursoft.harness.vec.v2x.VecPartOccurrence;
 import com.foursoft.harness.vec.v2x.VecTransformation3D;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -48,29 +50,36 @@ public class OccurrenceOrUsageViewItem3DTransformer
     @Override
     public TransformationResult<VecOccurrenceOrUsageViewItem3D> transform(final TransformationContext context,
                                                                           final ConnectionOrOccurrence source) {
-        final VecOccurrenceOrUsageViewItem3D destination = new VecOccurrenceOrUsageViewItem3D();
-        destination.setIdentification("ViewItem3D_" + source.getXmlId());
-
-        final TransformationResult.Builder<VecOccurrenceOrUsageViewItem3D> builder = TransformationResult.from(
-                destination);
-
         if (source instanceof final HasPlacement<?> hasPlacement) {
+            final VecOccurrenceOrUsageViewItem3D destination = new VecOccurrenceOrUsageViewItem3D();
+
+            if (source instanceof final HasIdentification hasIdentification && StringUtils.isNotBlank(
+                    hasIdentification.getId())) {
+                destination.setIdentification(hasIdentification.getId());
+            } else {
+                context.getLogger().warn("Occurrence (xml ID: {}) has no ID.", source.getXmlId());
+                destination.setIdentification("ViewItem3D_" + source.getXmlId());
+            }
+
+            final TransformationResult.Builder<VecOccurrenceOrUsageViewItem3D> builder = TransformationResult.from(
+                    destination);
+
+            if (source instanceof final HasAliasId<?> hasAliasId) {
+                final List<KblAliasIdentification> aliasIds = hasAliasId.getAliasIds().stream()
+                        .flatMap(StreamUtils.ofClass(KblAliasIdentification.class))
+                        .toList();
+                builder.withDownstream(KblAliasIdentification.class, VecAliasIdentification.class,
+                                       Query.fromLists(aliasIds), VecOccurrenceOrUsageViewItem3D::getAliasIds);
+            }
+
             final KblTransformation transformation = (KblTransformation) hasPlacement.getPlacement();
-            builder.withDownstream(KblTransformation.class, VecTransformation3D.class, Query.of(transformation),
-                                   VecOccurrenceOrUsageViewItem3D::setOrientation);
+            return builder
+                    .withDownstream(KblTransformation.class, VecTransformation3D.class, Query.of(transformation),
+                                    VecOccurrenceOrUsageViewItem3D::setOrientation)
+                    .withLinker(Query.of(source), VecPartOccurrence.class,
+                                VecOccurrenceOrUsageViewItem3D::getOccurrenceOrUsage)
+                    .build();
         }
-
-        if (source instanceof final HasAliasId<?> hasAliasId) {
-            final List<KblAliasIdentification> aliasIds = hasAliasId.getAliasIds().stream()
-                    .flatMap(StreamUtils.ofClass(KblAliasIdentification.class))
-                    .toList();
-            builder.withDownstream(KblAliasIdentification.class, VecAliasIdentification.class,
-                                   Query.fromLists(aliasIds), VecOccurrenceOrUsageViewItem3D::getAliasIds);
-        }
-
-        return builder
-                .withLinker(Query.of(source), VecPartOccurrence.class,
-                            VecOccurrenceOrUsageViewItem3D::getOccurrenceOrUsage)
-                .build();
+        return TransformationResult.noResult();
     }
 }
