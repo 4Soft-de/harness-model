12681af0f66985e1a4bd6cf86b85e06920962211
Yousif Yacoub <yacoub@4soft.de>
2025-10-28 15:19:06 +0100

    Add segment locations

--- kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/topology/placements/PlacementSpecificationTransformer.java
+++ kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/topology/placements/PlacementSpecificationTransformer.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,13 +26,15 @@
 package com.foursoft.harness.kbl2vec.transform.topology.placements;
 
 import com.foursoft.harness.kbl.v25.KblHarness;
-import com.foursoft.harness.kbl.v25.KblWireProtectionOccurrence;
+import com.foursoft.harness.kbl.v25.KblProtectionArea;
+import com.foursoft.harness.kbl2vec.core.Query;
 import com.foursoft.harness.kbl2vec.core.TransformationContext;
 import com.foursoft.harness.kbl2vec.core.TransformationResult;
 import com.foursoft.harness.kbl2vec.core.Transformer;
-import com.foursoft.harness.vec.v2x.VecOnPointPlacement;
 import com.foursoft.harness.vec.v2x.VecOnWayPlacement;
 import com.foursoft.harness.vec.v2x.VecPlacementSpecification;
+
+import java.util.List;
 
 public class PlacementSpecificationTransformer implements Transformer<KblHarness, VecPlacementSpecification> {
 
@@ -43,10 +45,14 @@ public class PlacementSpecificationTransformer implements Transformer<KblHarness
         destination.setIdentification("PLACEMENT");
 
         return TransformationResult.from(destination)
-                .withDownstream(KblWireProtectionOccurrence.class, VecOnPointPlacement.class,
-                                source::getWireProtectionOccurrences, VecPlacementSpecification::getPlacements)
-                .withDownstream(KblWireProtectionOccurrence.class, VecOnWayPlacement.class,
-                                source::getWireProtectionOccurrences, VecPlacementSpecification::getPlacements)
+                .withDownstream(KblProtectionArea.class, VecOnWayPlacement.class,
+                                Query.fromLists(protectionAreas(source)), VecPlacementSpecification::getPlacements)
                 .build();
+    }
+
+    private List<KblProtectionArea> protectionAreas(final KblHarness source) {
+        return source.getParentKBLContainer().getSegments().stream()
+                .flatMap(s -> s.getProtectionAreas().stream())
+                .toList();
     }
 }
