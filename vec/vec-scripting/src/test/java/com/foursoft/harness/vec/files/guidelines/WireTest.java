125f5ca148e0eebb1803928d6f07481ea4d74a36
Johannes Becker <johannes.becker@4soft.de>
2025-11-05 20:03:41 +0100

    Added some actual assertion for @RainerGanss ;-)

--- vec/vec-scripting/src/test/java/com/foursoft/harness/vec/files/guidelines/WireTest.java
+++ vec/vec-scripting/src/test/java/com/foursoft/harness/vec/files/guidelines/WireTest.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,9 +27,11 @@ package com.foursoft.harness.vec.files.guidelines;
 
 import com.foursoft.harness.vec.files.TestUtils;
 import com.foursoft.harness.vec.scripting.VecSession;
+import com.foursoft.harness.vec.v2x.VecContent;
 import com.foursoft.harness.vec.v2x.VecPrimaryPartType;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 class WireTest {
@@ -54,6 +56,18 @@ class WireTest {
         assertThatCode(() -> {
             session.writeToStream(TestUtils.createTestFileStream("ecad-wiki-guideline-single-core"));
         }).doesNotThrowAnyException();
+
+        final VecContent content = session.toVecContent();
+
+        assertThat(content).isNotNull()
+                .as("check VecContent").satisfies(vecContent -> {
+                    assertThat(vecContent.getDocumentVersions()).as("check documents")
+                            .hasSize(1);
+                    assertThat(vecContent.getPartVersions()).as("check parts")
+                            .hasSize(1);
+                })
+        ;
+
     }
 
 }
