a40590f1f67c133c44a4602405dd0296399e1023
DManstrator <DManstrator@gmail.com>
2025-11-06 13:00:11 +0100

    Re-added the previous VecValidation classes to avoid a breaking change
    The classes however have been marked as Deprecated for better notice.

---
+++ vec/vec-v2x/src/main/java/com/foursoft/harness/vec/v2x/validation/VecValidation.java
@@ -0,0 +1,59 @@
+/*-
+ * ========================LICENSE_START=================================
+ * VEC 2.X
+ * %%
+ * Copyright (C) 2020 - 2023 4Soft GmbH
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =========================LICENSE_END==================================
+ */
+package com.foursoft.harness.vec.v2x.validation;
+
+import javax.xml.validation.Schema;
+import java.util.function.Consumer;
+
+/**
+ * Validate VEC data.
+ *
+ * @deprecated Use {@link com.foursoft.harness.vec.common.validation.VecValidation} instead.
+ */
+@Deprecated(forRemoval = true)
+public final class VecValidation {
+
+    private VecValidation() {
+        // hide default constructor
+    }
+
+    /**
+     * Validates a xml string against the vec schema
+     *
+     * @param xmlContent  the xml content
+     * @param consumer    to display scheme violations.
+     * @param detailedLog if true and error happens a detailed log is written, use always true in tests !
+     * @deprecated Use {@link com.foursoft.harness.vec.common.validation.VecValidation#validateXML(Schema, String, Consumer, boolean)}
+     * with {@link SchemaFactory#getSchema()} instead.
+     */
+    @Deprecated(forRemoval = true)
+    public static void validateXML(final String xmlContent, final Consumer<String> consumer,
+                                   final boolean detailedLog) {
+        com.foursoft.harness.vec.common.validation.
+                VecValidation.validateXML(SchemaFactory.getSchema(), xmlContent, consumer, detailedLog);
+    }
+
+}
