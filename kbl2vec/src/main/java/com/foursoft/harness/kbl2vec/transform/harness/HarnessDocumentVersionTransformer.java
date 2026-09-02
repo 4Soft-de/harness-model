2a9d5826e06e56ffc3d141b427716af94a9033ec
Johannes Becker <johannes.becker@4soft.de>
2026-09-02 10:05:43 +0200

    kbl2vec: Added support for module families

--- kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/harness/HarnessDocumentVersionTransformer.java
+++ kbl2vec/src/main/java/com/foursoft/harness/kbl2vec/transform/harness/HarnessDocumentVersionTransformer.java
@@ -56,6 +56,8 @@ public class HarnessDocumentVersionTransformer implements Transformer<KblHarness
                                 VecDocumentVersion::getSpecifications)
                 .withDownstream(KblHarness.class, VecConfigurationConstraintSpecification.class, Query.of(source),
                                 VecDocumentVersion::getSpecifications)
+                .withDownstream(KblHarness.class, VecModuleFamilySpecification.class, Query.of(source),
+                                VecDocumentVersion::getSpecifications)
                 .withDownstream(KblHarness.class, VecHarnessDrawingSpecification2D.class, Query.of(source),
                                 VecDocumentVersion::getSpecifications)
                 .withDownstream(KblHarness.class, VecHarnessGeometrySpecification3D.class, Query.of(source),
